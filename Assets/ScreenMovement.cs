637028a61fc37538f565e652ff4a0a0ac2ed7571
Erick <19063989@student.hhs.nl>
2024-04-19 11:11:10 +0200

    added movement

---
+++ Assets/ScreenMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ScreenMovement : MonoBehaviour
+{
+    private float moveSpeed = 10;
+    private Vector2 vector;
+    private Vector3 moveDirection;
+    
+    void Update()
+    {
+        moveDirection = new(vector.x, 0f ,vector.y);
+        transform.Translate(moveSpeed * Time.deltaTime * moveDirection, Space.World);
+    }
+
+    void OnMove(InputValue value) => vector = value.Get<Vector2>();
+}
