cb3158846692837f53fba3c5496b8c44d068c67a
Erick <19063989@student.hhs.nl>
2024-04-19 09:22:34 +0200

    added first script

---
+++ Assets/Game_Manager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Game_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
