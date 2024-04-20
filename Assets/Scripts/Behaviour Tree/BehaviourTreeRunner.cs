356e598944cb5eb203c745f838a1968af0291930
Erick <19063989@student.hhs.nl>
2024-04-20 14:28:33 +0200

    Added delay

--- Assets/Scripts/Behaviour Tree/BehaviourTreeRunner.cs
+++ Assets/Scripts/Behaviour Tree/BehaviourTreeRunner.cs
@@ -10,15 +10,36 @@ public class BehaviourTreeRunner : MonoBehaviour
     void Start()
     {
         tree = ScriptableObject.CreateInstance<BehaviourTree>(); 
-               
-        var log = ScriptableObject.CreateInstance<DebugLogNode>();
-        log.message = "Hello Test message";
+
+        var log1 = ScriptableObject.CreateInstance<DebugLogNode>();
+        log1.message = "Hello 111";
+
+        var pause1 = ScriptableObject.CreateInstance<WaitNode>();
+
+        var log2 = ScriptableObject.CreateInstance<DebugLogNode>();
+        log2.message = "Hello 222";
+
+        var pause2 = ScriptableObject.CreateInstance<WaitNode>();
+
+        var log3 = ScriptableObject.CreateInstance<DebugLogNode>();
+        log3.message = "Hello 333";
+
+        var pause3 = ScriptableObject.CreateInstance<WaitNode>();
+
+        var sequence = ScriptableObject.CreateInstance<SequencerNode>();
+        sequence.children.Add(log1);
+        sequence.children.Add(pause1);
+        sequence.children.Add(log2);
+        sequence.children.Add(pause2);
+        sequence.children.Add(log3);
+        sequence.children.Add(pause3);
 
         var loop = ScriptableObject.CreateInstance<RepeatNode>();
-        loop.child = log;
+        loop.child = log1;
+        
 
         // the final thing it will execute
-        tree.rootNode = loop;
+        tree.rootNode = sequence;
     }
 
     // Update is called once per frame
