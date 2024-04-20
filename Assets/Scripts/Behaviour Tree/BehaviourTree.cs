4401935a3a74a1585785e6e942e28d4517e1bfed
Erick <19063989@student.hhs.nl>
2024-04-20 15:16:05 +0200

    Update BehaviourTree.cs

--- Assets/Scripts/Behaviour Tree/BehaviourTree.cs
+++ Assets/Scripts/Behaviour Tree/BehaviourTree.cs
@@ -7,8 +7,6 @@ public class BehaviourTree : ScriptableObject
     public Node rootNode;
     public Node.State treeState = Node.State.Running;
 
-    public List<Node> nodes = new List<Node>(); // you can always create a node and link it up, it can be detached.
-
     public Node.State Update()
     {
         if(rootNode.state == Node.State.Running)
