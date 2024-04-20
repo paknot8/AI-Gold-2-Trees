12eb5fe68e932c123b8768cc84f037842df8ef12
Erick <19063989@student.hhs.nl>
2024-04-20 14:07:20 +0200

    Added nodes new method

---
+++ Assets/Scripts/Behaviour Tree/BehaviourTreeRunning.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviourTreeRunning : MonoBehaviour
+{
+    BehaviourTree tree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tree = ScriptableObject.CreateInstance<BehaviourTree>();        
+        var log = ScriptableObject.CreateInstance<DebugLogNode>();
+        log.message = "Hello Test message";
+
+        tree.rootNode = log;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tree.Update();
+    }
+}
