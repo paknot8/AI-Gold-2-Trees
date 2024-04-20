3c220c567445fa988436c309f30e3bc7706d1f65
Erick <19063989@student.hhs.nl>
2024-04-20 12:49:28 +0200

    Update Tree.cs

--- Assets/Scripts/Example1/Tree.cs
+++ Assets/Scripts/Example1/Tree.cs
@@ -2,16 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// --- Generic Behaviour Tree --- //
 namespace BehaviorTrees
 {
-    public class Tree : MonoBehaviour
+    public abstract class Tree : MonoBehaviour
     {
+        // #12
         // reference to a root itself recursively contains the entire tree
         private Node _root = null; 
 
+        // #13
+        // Two Things
+        // Upon start the tree Class will build the behaviour tree according to the setup tree function defined
         protected void Start()
         {
             _root = SetupTree();
         }
+
+        // #14
+        // If it has a tree it will evaluate it continuesly
+        private void Update()
+        {
+            if (_root != null)
+            {
+                _root.Evaluate();
+            }
+        }
+
+        protected abstract Node SetupTree();
+    
+        // after this prepare 2 Composite Nodes
     }
 }
