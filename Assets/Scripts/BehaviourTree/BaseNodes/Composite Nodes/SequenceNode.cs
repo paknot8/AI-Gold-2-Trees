69c5f23ddb3a7a5c7f0cd33bc07a2ed8f8d130ae
Erick <19063989@student.hhs.nl>
2024-04-25 22:45:04 +0200

    Added node files

--- Assets/Scripts/BehaviourTree/BaseNodes/Composite Nodes/SequenceNode.cs
+++ Assets/Scripts/BehaviourTree/BaseNodes/Composite Nodes/SequenceNode.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// This is a Composite Node
 public class SequenceNode : IBaseNode
 {
     private List<IBaseNode> children;
