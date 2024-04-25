1a5c1aeaad17c7d99dd05be385dbf8ce89b55dbf
Erick <19063989@student.hhs.nl>
2024-04-25 19:44:37 +0200

    Clean

--- Assets/Scripts/Behaviour Tree/Nodes/CompositeNode.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class CompositeNode : BaseNode
-{
-    public List<BaseNode> children = new List<BaseNode>();
-}
