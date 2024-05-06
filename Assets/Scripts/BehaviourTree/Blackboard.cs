485eaf51a5e320d1bd336fc3f5740bed64dc0aee
Erick <19063989@student.hhs.nl>
2024-05-06 16:32:26 +0200

    Added Blackboard

---
+++ Assets/Scripts/BehaviourTree/Blackboard.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using TMPro;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Blackboard : MonoBehaviour
+{
+    public static Blackboard instance;
+    private Player player;
+    public TextMeshProUGUI indicatorText;
+    public GameObject gameOverScreen;
+    public GameObject victoryScreen;
+
+    private void Awake()
+    {
+        instance = this;
+        player = FindObjectOfType<Player>();
+        indicatorText = GameObject.Find("IndicatorText").GetComponent<TextMeshProUGUI>();
+    }
+
+    public Vector3 GetPlayerPosition()
+    {
+        if (player != null)
+        {
+            return player.transform.position;
+        }
+        else
+        {
+            Debug.LogError("Player_Manager not found!");
+            return Vector3.zero;
+        }
+    }
+
+    public void SetIndicatorText(string newText)
+    {
+        if (indicatorText != null)
+        {
+            indicatorText.text = newText;
+        }
+        else
+        {
+            Debug.LogError("IndicatorText not assigned!");
+        }
+    }
+}
